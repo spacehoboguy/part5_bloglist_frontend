ff2bef38c06a1c95da2a7858eb37afb85d34f0bb
spacehoboguy <ph.olafsen@gmail.com>
2023-10-21 17:09:30 +0200

    add: tests for blogForm

---
+++ src/tests/BlogForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import BlogForm from '../components/BlogForm'
+import userEvent from '@testing-library/user-event'
+
+describe('<BlogForm/>', () => {
+
+})
