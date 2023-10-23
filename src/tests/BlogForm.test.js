e67a7138305dc4b69ee24d203200f353624bfb69
spacehoboguy <ph.olafsen@gmail.com>
2023-10-23 11:30:23 +0200

    add: unit test for BlogForm.jsx

--- src/tests/BlogForm.test.js
+++ src/tests/BlogForm.test.js
@@ -7,5 +7,21 @@ import userEvent from '@testing-library/user-event'
 describe('<BlogForm/>', () => {
 test('should pass correct details, when event handlers are called', async () => { 
     const createBlog = jest.fn()
+    const user = userEvent.setup()
+
+    render(<BlogForm createBlog={createBlog} />)
+
+    const titleInput = screen.getByPlaceholderText('add title...')
+    await user.type(titleInput, 'testTitle')
+    const authorInput = screen.getByPlaceholderText('add author...')
+    await user.type(authorInput, 'testAuthor')
+    const urlInput = screen.getByPlaceholderText('add url...')
+    await user.type(urlInput, 'testUrl')
+
+    const saveButton = screen.getByText('Create')
+    await user.click(saveButton)
+
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0]).toStrictEqual({'title':'testTitle', 'author':'testAuthor', 'url':'testUrl'})
  })
 })
